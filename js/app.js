6ab078f1f4780019849816ae618eb9f4ad9ce4c2
asmaembr <asmae.moubr@gmail.com>
2023-11-09 16:58:40 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -23,18 +23,27 @@ $(document).ready(function(){
         element: document.getElementById('exp-id'),
         handler: function() {
     var p =document.querySelectorAll('.progress-bar');
-    p[0].setAttribute("style","width:95%;transition:1s all");
-    p[1].setAttribute("style","width:70%;transition:1.5s all");
+    p[0].setAttribute("style","width:90%;transition:1s all");
+    p[1].setAttribute("style","width:60%;transition:1.5s all");
     p[2].setAttribute("style","width:88%;transition:1.7s all");
     p[3].setAttribute("style","width:60%;transition:2s all");
-    p[4].setAttribute("style","width:95%;transition:2.3s all");
-    p[5].setAttribute("style","width:90%;transition:2.5s all");
-    p[6].setAttribute("style","width:70%;transition:2.7s all");
+    p[4].setAttribute("style","width:80%;transition:2.3s all");
+    p[5].setAttribute("style","width:85%;transition:2.5s all");
+    p[6].setAttribute("style","width:60%;transition:2.7s all");
 
         },
 
         offset:'90%'
 
       });
+      //owl carousel
+ 
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:4000,
+            items:1,
+
+        });
 
 });
